9f6b4d21d5da2957da6f85b3e4c8e1c42ddf727a
Khib0o <louisteys31@gmail.com>
2023-11-03 06:04:50 +0000

    cleanup and beginnig styling

--- webServer.py
+++ webServer.py
@@ -2,7 +2,9 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 from ssl import wrap_socket
 
 host = "127.0.0.1"
-port = 8084
+port = 8081
+html_loc = "/home/ubuntu/basicPythonWebServer/src/"
+
 
 def sendFile(fileName, server):
     with open(fileName, "r") as file:
@@ -13,12 +15,31 @@ def sendFile(fileName, server):
 class MyServer(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        print(self.path)
+                
         self.send_response(200)
         self.send_header("Content-type", "text/html")
         self.end_headers()
+        
+        path = self.path
+        path = path.split("/")
 
-        sendFile("index.html", self)
+        match path[1]:
+            case "test1":
+                sendFile(html_loc+"test1.html", self)
+                pass
+
+            case "test2":
+                sendFile(html_loc+"test2.html", self)
+                pass
+
+            case "kill":
+                print("Kill request received, shutting dowm")
+                self.server.shutdown()
+                pass
+
+            case _:
+                sendFile(html_loc+"index.html", self)
+
 
 if __name__ == "__main__":
     webServer = HTTPServer((host, port), MyServer)
@@ -33,6 +54,7 @@ if __name__ == "__main__":
         webServer.serve_forever()
     except KeyboardInterrupt:
         pass
+        
 
     webServer.server_close()
     print("Server stopped.")
